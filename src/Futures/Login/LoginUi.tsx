8341c588139c9c8a6fd697605db64995ffef9082
Ahmed-Nasser99 <ahmednsmd@gmail.com>
2023-11-26 12:57:38 +0200

    Setup Login Ui

--- src/Futures/Login/LoginUi.tsx
+++ src/Futures/Login/LoginUi.tsx
@@ -2,21 +2,44 @@ import React from "react";
 
 export default function LoginUi() {
   return (
-    <form>
-      <div className="input-group mb-3">
-        <span className="input-group-text">
-          <i className="bi bi-envelope"></i>
-        </span>
-        <div className="form-floating">
-          <input
-            type="text"
-            className="form-control"
-            id="floatingInputGroup1"
-            placeholder="Username"
-          />
-          <label htmlFor="floatingInputGroup1">Username</label>
+    <div className="row h-100 align-items-center justify-content-center">
+      <form>
+        <h3 className="text-center text-info">Login</h3>
+        <div className="input-group mb-3">
+          <span className="input-group-text bg-info text-white">
+            <i className="bi bi-envelope"></i>
+          </span>
+          <div className="form-floating">
+            <input
+              type="email"
+              className="form-control"
+              id="floatingInputGroup1"
+              placeholder="Email"
+            />
+            <label htmlFor="floatingInputGroup1">Email</label>
+          </div>
         </div>
-      </div>
-    </form>
+        <div className="input-group mb-3">
+          <span className="input-group-text bg-info text-white">
+            <i className="bi bi-key"></i>
+          </span>
+          <div className="form-floating">
+            <input
+              type="password"
+              className="form-control"
+              id="floatingInputGroup1"
+              placeholder="Password"
+            />
+            <label htmlFor="floatingInputGroup1">Password</label>
+          </div>
+        </div>
+        <button
+          className="btn btn-info text-white w-100 rounded-5"
+          type="submit"
+        >
+          Login
+        </button>
+      </form>
+    </div>
   );
 }
